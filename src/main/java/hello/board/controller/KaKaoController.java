3611bc93ab8a09a22da38957fa2ce234b6ce5ee6
Geondol <keedob@naver.com>
2022-10-20 22:46:28 +0900

    파일 업로드 기능

--- src/main/java/hello/board/controller/KaKaoController.java
+++ src/main/java/hello/board/controller/KaKaoController.java
@@ -30,7 +30,6 @@ public class KaKaoController {
         HashMap<String,Object> userInfo = kaKaoService.getUserInfo(access_Token);
         System.out.println("login Controller = " + userInfo);
         model.addAttribute("email",userInfo.get("email"));
-
         //클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
         if (userInfo.get("email") != null){
             session.setAttribute("userId", userInfo.get("email"));
